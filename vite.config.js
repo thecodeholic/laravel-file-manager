41d58241de7af5974e3a24f1e97ad8d5e0cdc059
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2023-04-17 20:46:42 +0400

    Fix bug in vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    server: {
+        hmr: {
+            host: 'localhost'
+        }
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.js',
