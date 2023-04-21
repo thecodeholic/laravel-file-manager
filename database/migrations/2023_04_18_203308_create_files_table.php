5bd99a466780ca03dce7b72e24f49cf8c77522a9
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2023-04-22 02:15:19 +0400

    Install nested set model and create migrations and models

---
+++ database/migrations/2023_04_18_203308_create_files_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 1024);
+            $table->string('slug', 1024);
+            $table->nestedSet();
+            $table->boolean('is_folder');
+            $table->string('mime');
+            $table->integer('size');
+            $table->timestamps();
+            $table->foreignIdFor(\App\Models\User::class, 'created_by');
+            $table->foreignIdFor(\App\Models\User::class, 'updated_by');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('file_folders');
+    }
+};
---
+++ database/migrations/2023_04_21_035938_create_starred_files_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('starred_files', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('file_id')->nullable()->constrained('files');
+            $table->foreignId('user_id')->nullable()->constrained('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('starred_files');
+    }
+};
