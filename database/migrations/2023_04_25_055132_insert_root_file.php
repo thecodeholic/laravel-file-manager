9fd1822cc107bdfd837aaccebdd9a2578d9fec0f
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2023-04-25 22:08:52 +0400

    Create new migration to insert root record in files table

---
+++ database/migrations/2023_04_25_055132_insert_root_file.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::table('files')->insert([
+            'name' => 'root',
+            'slug' => 'root',
+            '_lft' => 1,
+            '_rgt' => 2,
+            'is_folder' => 1,
+            'created_by' => 2,
+            'updated_by' => 2
+        ]); // Saved as root
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::table('files')
+            ->where('name', '=', 'root')
+            ->where('_lft', '=', 1)
+            ->delete();
+    }
+};
