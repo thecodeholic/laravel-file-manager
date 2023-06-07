550033cfae6fdd7187ddc90875a534562e4b7a62
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2023-06-07 23:03:43 +0400

    Redirect to my-files from /

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,7 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return redirect()->route('myFiles');
 });
 
 Route::controller(\App\Http\Controllers\FileController::class)
