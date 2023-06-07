5f80afcd66fb64afb0c23c8c8850177c09093a61
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2023-06-07 22:40:52 +0400

    Working on uploading files on s3.

--- app/Jobs/UploadFileToCloudJob.php
+++ app/Jobs/UploadFileToCloudJob.php
@@ -44,14 +44,19 @@ class UploadFileToCloudJob implements ShouldQueue
 
         if (!$model->uploaded_on_cloud) {
             $localPath = Storage::disk('local')->path($model->storage_path);
-            Log::debug('Uploading on S3... '.$localPath);
-            Storage::copy($model->storage_path,  new \Illuminate\Http\File($localPath));
-//            Log::debug('Deleting local FS file...');
-//            Storage::disk('local')->delete($model->storage_path);
-            Log::debug('Updating the database');
-            $model->uploaded_on_cloud = 1;
-            $model->save();
+            Log::debug('Uploading on S3... ' . $localPath);
+            try {
+                $success = Storage::put($model->storage_path, Storage::disk('local')->get($model->storage_path));
+                if ($success) {
+                    Log::debug('Updating the database ');
+                    $model->uploaded_on_cloud = 1;
+                    $model->save();
+                } else {
+                    Log::error('Unable to upload files to S3');
+                }
+            } catch (\Exception $e) {
+                Log::error($e->getMessage());
+            }
         }
-
     }
 }
